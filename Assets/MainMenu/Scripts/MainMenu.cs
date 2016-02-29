5f2d098be02240a2e81f70ff36b64f93f7173a3c
Sam Calmes <Sam Calmes>
2016-02-29 15:31:56 -0600

    Adding in some directory structure

---
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+// This is a script for the Main Menu of our game.
+
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI(){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
